923ae245b1c101c0b4eaf43c61decebf17365201
Ondřej Stejskal <71814723+ostejskal@users.noreply.github.com>
2024-09-24 10:12:29 +0200

    Nahrání C# kódu
    Tvorba aplikace ve VS jazyk C# (formulář).

---
+++ WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rng = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cc = (int)numericUpDown1.Value;
+            MessageBox.Show(rng.Next(1, cc + 1).ToString());
+        }
+    }
+}
